3ec015aee7884a6ae174a69e0ed012f438ea6c7a
ViktorV <Fjo22524@gmail.com>
2022-11-14 20:50:25 +0100

    weather-wetter

--- Simple_Weather/111/weather/app/forms.py
+++
@@ -1,9 +0,0 @@
-from .models import City
-from django.forms import ModelForm, TextInput
-
-class CityForm(ModelForm):
-    class Meta:
-        model = City
-        fields = ['name']
-        widgets = {'name' : TextInput(attrs={'class': 'input', 'type':'text', 'placeholder': 'City Name', "id":'city'})}
-        
