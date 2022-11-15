37ce1bab1b1f08ab825cc7b49c2d617bca16d605
ViktorV <Fjo22524@gmail.com>
2022-11-15 19:42:21 +0100

    weather-wetter

--- weather/app/views.py
+++ weather/app/views.py
@@ -7,19 +7,16 @@ appid = "ce9a769fe69bd36df32f6627d9923114"
 api_url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
 
 
-
 def index(request):
     if (request.method == 'POST'):
         form = CityForm(request.POST)
         form.save() 
-        form = CityForm()
-
+    form = CityForm()
+    all_cities = []
     try:  
-        all_cities = []
         cities = City.objects.all()
         for city in cities:
             res = requests.get(api_url.format(city)).json()
-
             city_info = {
                 'city' : city.name,   
                     'temp_now' : res["main"]["feels_like"],
